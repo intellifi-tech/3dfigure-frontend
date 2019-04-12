d923903dab8dd6c93a51adb95a548c6d4d05a616
edceo <yusufonder_55@hotmail.com>
2019-04-12 18:52:31 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,52 @@
+/*=========================================================================================
+  File Name: main.js
+  Description: main vue(js) file
+  ----------------------------------------------------------------------------------------
+  Item Name: Vuesax Admin - VueJS Dashboard Admin Template
+  Version: 1.1
+  Author: Pixinvent
+  Author URL: hhttp://www.themeforest.net/user/pixinvent
+==========================================================================================*/
+
+
+import Vue from 'vue'
+import App from './App.vue'
+
+// Vuesax Component Framework
+import Vuesax from 'vuesax'
+import 'material-icons/iconfont/material-icons.css' //Material Icons
+import 'vuesax/dist/vuesax.css'; // Vuesax
+Vue.use(Vuesax)
+
+// Theme Configurations
+import '../themeConfig.js'
+
+// Globally Registered Components
+import './globalComponents.js'
+
+// Styles: SCSS
+import './assets/scss/main.scss'
+
+// Tailwind
+import '@/assets/css/main.css';
+
+// Vue Router
+import router from './router'
+
+// Vuex Store
+import store from './store/store'
+
+// PrismJS
+import 'prismjs'
+import 'prismjs/themes/prism.css'
+
+// Feather font icon
+require('./assets/css/iconfont.css')
+
+Vue.config.productionTip = false
+
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app')
