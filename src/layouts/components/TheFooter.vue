68745e26bec475f100f11bb6d393a4f731cf9032
nebhenaytas <nebhenaytas@gmail.com>
2019-07-10 16:33:17 +0300

    Genel Fix 4 Update
    -arkadas davet etme popup eklendi
    -landing page "Fiyatlandırma" alanı "hidden" yapıldı ve menüden kaldırıldı
    -Bilgilendirme panosu güncellendi
    -Dashboard da footer intelliFi kaldırıldı

--- src/layouts/components/TheFooter.vue
+++ src/layouts/components/TheFooter.vue
@@ -12,7 +12,7 @@
 
 <template>
 	<footer class="the-footer flex-wrap justify-between" :class="classes">
-		<span>© 2019 3D Figür. Tüm hakları saklıdır. <a href="https://intellifi.tech" target="_blank" rel="nofollow">IntelliFi</a></span>
+		<span>© 2019 3D Figür. Tüm hakları saklıdır. <!--<a href="https://intellifi.tech" target="_blank" rel="nofollow">IntelliFi</a>--></span>
 	<!--	<span class="md:flex hidden items-center">
 			<span>Hand-crafted & Made with</span>
 			<feather-icon icon="HeartIcon" svgClasses="stroke-current text-danger w-6 h-6" class="ml-2" />
